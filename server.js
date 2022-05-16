3ff3e0eaaad4cc8d72724fdda1e6dcb1b75e931d
Dimitar Prisadnikov <i454903@fhict.nl>
2022-05-17 00:35:02 +0300

    changed cors settings

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.json({limit: '50mb'}));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://i454903.hera.fhict.nl");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
